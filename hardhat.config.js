1f3db6a6cd6ff29bbe8427bea74cdadebbab9dc9
Mathieu <70205357+MathieuBurnat@users.noreply.github.com>
2022-12-05 16:23:51 +0100

    add files

---
+++ hardhat.config.js
@@ -0,0 +1,19 @@
+require("@nomicfoundation/hardhat-toolbox");
+require('dotenv').config();
+require('@nomiclabs/hardhat-ethers')
+
+const { alchemy_api_url, wallet_private_key } = process.env;
+
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  solidity: "0.8.17",
+  defaultNetwork: "goerli",
+
+  networks: {
+    hardhat: {},
+    goerli: {
+      url: alchemy_api_url,
+      accounts: [`0x${wallet_private_key}`],
+    }
+  }
+};
